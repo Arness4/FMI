52531cbf3fbf3ba2f255738616ea1f93b98e7d23
MAMPODY Nasser Ext OBS/GDO <kfyj0488@orange.com>
2025-07-30 12:50:33 +0200

    initial

--- app.py
+++ app.py
@@ -113,5 +113,9 @@ def get_unique_values():
         'inviteurs': [i[0] for i in inviteurs]
     })
 
+@app.route('/', methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
